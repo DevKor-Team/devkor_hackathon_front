b0cf08806bdab867b39f87efeb430e01bffb0c39
froggagul <froggagul@korea.ac.kr>
2021-05-16 01:42:00 +0900

    feat: add modal action to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,56 @@
 import React from 'react';
 import { useMediaQuery } from 'react-responsive';
+import LoginModal from './Login';
 
 const logo = 'images/containers/Navbar/devkor_logo.svg';
 
 export const DesktopNavbar = () => {
+  const [isLoginModalOn, setIsLoginModalOn] = React.useState(false);
+  const [isAuthModalOn, setIsAuthModalOn] = React.useState(false);
+
+  const toggleModal = (type) => {
+    if (type === 'login') {
+      setIsLoginModalOn(!isLoginModalOn);
+    } else if (type === 'signup') {
+      setIsAuthModalOn(!isAuthModalOn);
+    }
+  };
   return (
     <>
+      <LoginModal isOn={isLoginModalOn} />
       <div className="navbar__container">
         <div className="logo">
           <img src={logo} alt="devkor" />
         </div>
         <ul>
-          <li> LOGIN </li>
-          <li> SIGNUP </li>
+          <li>
+            <div
+              onClick={() => {
+                toggleModal('login');
+              }}
+              onKeyDown={(e) => {
+                if (e.key === 'Enter') toggleModal('login');
+              }}
+              role="button"
+              tabIndex={0}
+            >
+              LOGIN
+            </div>
+          </li>
+          <li>
+            <div
+              onClick={() => {
+                toggleModal('signup');
+              }}
+              onKeyDown={(e) => {
+                if (e.key === 'Enter') toggleModal('signup');
+              }}
+              role="button"
+              tabIndex={0}
+            >
+              SIGNUP
+            </div>
+          </li>
           <li> PROJECTS </li>
           <li> ABOUT </li>
         </ul>
