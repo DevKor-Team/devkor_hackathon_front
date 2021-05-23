f47c8370cf2a2f4321ca2cae910aeb276b738d19
junhyeog <junhyeog@korea.ac.kr>
2021-05-24 02:59:23 +0900

    feat: teampage delete popup

--- src/containers/Team/TeamContainer.js
+++ src/containers/Team/TeamContainer.js
@@ -25,22 +25,49 @@ ButtonItem.propTypes = {
   onClick: PropTypes.func,
 };
 
-export const TeamContainer = () => {
+const DeletePopup = ({ onClickY, onClickN }) => {
   return (
-    <div className={styles.team__container}>
-      <div className={styles.team__container__title}>팀명 : DevKor</div>
-      <div className={styles.team__container__membertitle}>멤버 구성(4)</div>
-      <div className={styles.team__container__memberwrapper}>
-        <MemberItem title="TaehoLee" sub="Leader" onClick={defaultOnClick} />
-        <MemberItem title="L0z1k" sub="Member" onClick={defaultOnClick} />
-        <MemberItem title="froggagul" sub="Member" onClick={defaultOnClick} />
-        <MemberItem title="bbangjo" sub="Member" onClick={defaultOnClick} />
-      </div>
-      <div className={styles.team__container__buttonwrapper}>
-        <ButtonItem text="팀 초대링크 복사하기" onClick={defaultOnClick} />
-        <ButtonItem text="팀 탈퇴하기" onClick={defaultOnClick} />
+    <div className={styles.team__container__deletepopup}>
+      <div className={styles.team__container__deletepopup__container}>
+        <div className={styles.team__container__deletepopup__container__title}>
+          정말 탈퇴하시겠습니까?
+        </div>
+        <div className={styles.team__container__deletepopup__container__buttonwrapper}>
+          <ButtonItem text="예" onClick={onClickY} />
+          <ButtonItem text="아니요" onClick={onClickN} />
+        </div>
       </div>
     </div>
+  );
+};
+
+DeletePopup.propTypes = {
+  onClickY: PropTypes.func,
+  onClickN: PropTypes.func,
+};
+
+export const TeamContainer = () => {
+  const [popup, setPopup] = React.useState(false);
+  return (
+    <>
+      <div className={styles.team__container}>
+        <div className={styles.team__container__title}>팀명 : DevKor</div>
+        <div className={styles.team__container__membertitle}>멤버 구성(4)</div>
+        <div className={styles.team__container__memberwrapper}>
+          <MemberItem title="TaehoLee" sub="Leader" onClick={defaultOnClick} />
+          <MemberItem title="L0z1k" sub="Member" onClick={defaultOnClick} />
+          <MemberItem title="froggagul" sub="Member" onClick={defaultOnClick} />
+          <MemberItem title="bbangjo" sub="Member" onClick={defaultOnClick} />
+        </div>
+        <div className={styles.team__container__buttonwrapper}>
+          <ButtonItem text="팀 초대링크 복사하기" onClick={defaultOnClick} />
+          <ButtonItem text="팀 탈퇴하기" onClick={() => setPopup((curVal) => !curVal)} />
+        </div>
+      </div>
+      {popup ? (
+        <DeletePopup onClickY={defaultOnClick} onClickN={() => setPopup((curVal) => !curVal)} />
+      ) : null}
+    </>
   );
 };
 
