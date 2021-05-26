eda4c518a15c6912ad58c7c43dd16dc694c6954b
KIMMINSEOK <remigailard80@korea.ac.kr>
2021-05-26 11:16:43 +0900

    refactor: scss bem 삭제

--- src/containers/My/MyContainer.js
+++ src/containers/My/MyContainer.js
@@ -11,8 +11,8 @@ const defaultOnClick = () => {
 
 const InfoItem = ({ title }) => {
   return (
-    <div className={styles.my__container__info}>
-      <div className={styles.my__container__info__text}>{title}</div>
+    <div className={styles.info}>
+      <div className={styles.info__text}>{title}</div>
     </div>
   );
 };
@@ -23,8 +23,8 @@ InfoItem.propTypes = {
 
 const ButtonItem = ({ text, onClick }) => {
   return (
-    <div className={styles.my__container__button} onClick={onClick} role="button" tabIndex={0}>
-      <div className={styles.my__container__button__text}>{text}</div>
+    <div className={styles.button} onClick={onClick} role="button" tabIndex={0}>
+      <div className={styles.button__text}>{text}</div>
     </div>
   );
 };
@@ -38,18 +38,18 @@ export const MyContainer = () => {
   const [popup, setPopup] = React.useState(false);
   return (
     <>
-      <div className={styles.my__container}>
-        <div className={styles.my__container__imagewrapper}>
+      <div className={styles.container}>
+        <div className={styles.imagewrapper}>
           <img src={defaultimg} alt="profile" />
         </div>
-        <div className={styles.my__container__title}>Hojin Jung</div>
-        <div className={styles.my__container__infowrapper}>
+        <div className={styles.title}>Hojin Jung</div>
+        <div className={styles.infowrapper}>
           <InfoItem title="email" />
           <InfoItem title="실명" />
           <InfoItem title="github 주소" />
           <InfoItem title="Position" />
         </div>
-        <div className={styles.my__container__buttonwrapper}>
+        <div className={styles.buttonwrapper}>
           <ButtonItem text="수정하기" onClick={() => setPopup((curVal) => !curVal)} />
         </div>
       </div>
