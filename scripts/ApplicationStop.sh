4f85ecf97a1c2f54560a54e1e7a31032802cf7bc
froggagul <froggagul@korea.ac.kr>
2021-06-26 17:39:25 +0900

    fix: kill app with pm2

--- scripts/ApplicationStop.sh
+++ scripts/ApplicationStop.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 #stop existing node servers
-pid=`sudo netstat -tnlp | grep :3000 | awk '{ print $7 }' | cut -f 1 -d '/'`
-kill -9 $pid
+export NVM_DIR="$HOME/.nvm"	
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
+
+pm2 delete next
