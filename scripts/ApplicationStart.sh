119a037b33e4545c1f50f493a7ad940cf38018e8
froggagul <froggagul@korea.ac.kr>
2021-06-26 19:16:27 +0900

    fix: give permission when application start

--- scripts/ApplicationStart.sh
+++ scripts/ApplicationStart.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# give permission
+DIR="/home/ec2-user/hackathon/front"
+chmod -R u+w ${DIR}
+
+
 export NVM_DIR="$HOME/.nvm"	
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
