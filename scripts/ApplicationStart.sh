7355e5d0ecbebab24d9f3feb21415dd78374fdff
froggagul <froggagul@korea.ac.kr>
2021-06-26 16:04:35 +0900

    fix: application start needs node path

--- scripts/ApplicationStart.sh
+++ scripts/ApplicationStart.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+export NVM_DIR="$HOME/.nvm"	
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
+
+
 # insall modules
 yarn
 
