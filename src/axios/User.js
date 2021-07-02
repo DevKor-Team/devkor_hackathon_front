07c75078d4ce3d8f4249dc7364a5b63d37b9d696
김민석 <kimminseok@gimminseog-ui-MacBookAir.local>
2021-07-02 21:42:53 +0900

    feat: navbar Changed, logout

--- src/axios/User.js
+++ src/axios/User.js
@@ -19,7 +19,17 @@ export function getUserInfo() {
     }
   );
 }
-
+export function Logout() {
+  return axios(
+    {
+      method: 'POST',
+      url: '/api/oauth/logout/',
+    },
+    {
+      withCredentials: true,
+    }
+  );
+}
 export function putUserProfile(data, id) {
   return axios(
     {
