00ad6721d60373136466b2c4530fc436125b8220
froggagul <froggagul@korea.ac.kr>
2021-08-19 23:46:27 +0900

    fix: modal conflict

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -2,11 +2,33 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from 'styles/components/modal.module.scss';
 
-const Modal = ({ children, turnOff }) => {
+const Modal = ({ children, isOn, turnOff }) => {
+  return (
+    isOn && (
+      <>
+        <div
+          className={styles.modalBackground}
+          onClick={(e) => {
+            e.stopPropagation();
+            e.nativeEvent.stopImmediatePropagation();
+            if (e.target === e.currentTarget) {
+              turnOff();
+            }
+          }}
+          role="dialog"
+        >
+          {children}
+        </div>
+      </>
+    )
+  );
+};
+
+export const LoginModal = ({ children, turnOff }) => {
   return (
     <>
       <div
-        className={styles.background}
+        className={styles.loginModalBackground}
         onClick={() => {
           turnOff();
         }}
@@ -17,8 +39,14 @@ const Modal = ({ children, turnOff }) => {
   );
 };
 
+LoginModal.propTypes = {
+  children: PropTypes.elementType.isRequired,
+  turnOff: PropTypes.func,
+};
+
 Modal.propTypes = {
   children: PropTypes.elementType.isRequired,
+  isOn: PropTypes.bool,
   turnOff: PropTypes.func,
 };
 
--- src/pages/demo/[id]/index.js
+++ src/pages/demo/[id]/index.js
@@ -7,6 +7,24 @@ import { ErrorContainer } from 'containers/Error/ErrorContainer';
 import { LoadingSpinner } from 'components/Loading';
 import { useDispatch } from 'react-redux';
 
+// export async function getStaticProps(context) {
+//   const { id } = context.params;
+//   const result = await axios
+//     .get(`http://localhost:3000/api/demo/demo/${id}`)
+//     .catch((error) => error && error.response && error.response.status);
+//   const data = typeof (await result) === 'number' ? result : result.data;
+//   return {
+//     props: { data },
+//   };
+// }
+
+// export async function getStaticPaths() {
+//   return {
+//     paths: [],
+//     fallback: true,
+//   };
+// }
+
 export default function Demo({ data }) {
   const router = useRouter();
   const dispatch = useDispatch();
