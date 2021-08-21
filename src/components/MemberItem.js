0de991b2fa5db64dac6889cf1e83f0b5fd0a4660
hanchchch <cndghks15@gmail.com>
2021-08-21 12:04:25 +0900

    feat: team page profile

--- src/components/MemberItem.js
+++ src/components/MemberItem.js
@@ -3,14 +3,15 @@ import styles from 'styles/components/memberItem.module.scss';
 
 const memberItemIamg = '/images/components/MemberItem/default.jpg';
 
-export const MemberItem = ({ title, sub, onClick }) => {
+export const MemberItem = ({ title, sub, profile, onClick }) => {
   return (
     <div className={styles.memberItem__container} onClick={onClick} role="button" tabIndex={0}>
       <div className={styles.memberItem__imagewrapper}>
-        <img src={memberItemIamg} alt="profile" />
+        <img src={profile?.profile_img ?? memberItemIamg} alt="profile" />
       </div>
       <div className={styles.memberItem__textwrapper}>
         <p className={styles.memberItem__textwrapper__title}>{title}</p>
+        <p className={styles.memberItem__textwrapper__position}>{profile?.position}</p>
         <p className={styles.memberItem__textwrapper__sub}>{sub}</p>
       </div>
     </div>
@@ -21,6 +22,10 @@ MemberItem.propTypes = {
   title: PropTypes.string,
   sub: PropTypes.string,
   onClick: PropTypes.func,
+  profile: PropTypes.shape({
+    position: PropTypes.string,
+    profile_img: PropTypes.string,
+  }),
 };
 
 export default MemberItem;
